f0ffeaa3e8793236c2e702b1447720261531a18e
sovietbehemoth <73245562+sovietbehemoth@users.noreply.github.com>
2021-03-22 21:01:42 -0400

    Create keys.h

---
+++ keys.h
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define ALPHA_A 38
+#define ALPHA_B 56
+#define ALPHA_C 54
+#define ALPHA_D 40
+#define ALPHA_E 26
+#define ALPHA_F 41
+#define ALPHA_G 42
+#define ALPHA_H 43
+#define ALPHA_I 31
+#define ALPHA_J 44
+#define ALPHA_K 45
+#define ALPHA_L 46
+#define ALPHA_M 58
+#define ALPHA_N 57
+#define ALPHA_O 32
+#define ALPHA_P 33
+#define ALPHA_Q 24
+#define ALPHA_R 27
+#define ALPHA_S 39
+#define ALPHA_T 28
+#define ALPHA_U 30
+#define ALPHA_V 55
+#define ALPHA_W 25
+#define ALPHA_X 53
+#define ALPHA_Y 29
+#define ALPHA_Z 52
+
+#define SPACE_BAR 65
+#define ENTER_KEY 36
+#define CONTROL_KEY 37
+
+#define INTEGER_1 10
+#define INTEGER_2 11
+#define INTEGER_3 12
+#define INTEGER_4 13
+#define INTEGER_5 14
+#define INTEGER_6 15
+#define INTEGER_7 16
+#define INTEGER_8 17
+#define INTEGER_9 18
+#define INTEGER_0 19
